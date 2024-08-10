a0424cc053e576e1cb13a814b7e6ff2203a982f7
nipun-das <nipundas02@gmail.com>
2024-08-10 20:11:50 +0530

    final commits - consumer - completed

--- Kafka-Project/src/main/java/com/spring_kafka_microservices/SpringKafka/kafka/KafkaProducerJSON.java
+++ Kafka-Project/src/main/java/com/spring_kafka_microservices/SpringKafka/kafka/KafkaProducerJSON.java
@@ -3,6 +3,7 @@ package com.spring_kafka_microservices.SpringKafka.kafka;
 import com.spring_kafka_microservices.SpringKafka.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -11,6 +12,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerJSON {
+
+    @Value("${spring.kafka.topic-json.name}")
+    private String topicJsonName;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
     private KafkaTemplate<String,String> kafkaTemplate;
 
@@ -24,7 +29,7 @@ public class KafkaProducerJSON {
 
         Message<User> message= MessageBuilder
                 .withPayload(data)
-                        .setHeader(KafkaHeaders.TOPIC,"messages")
+                        .setHeader(KafkaHeaders.TOPIC,topicJsonName)
                                 .build();
 
         kafkaTemplate.send(message);
